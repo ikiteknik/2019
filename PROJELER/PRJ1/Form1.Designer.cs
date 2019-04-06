5193e7f5f89c1f6629ca326557d8a86ad31d82f3
pchsyn <pchsyn@DESKTOP-H5G5TTM>
2019-04-06 22:58:45 +0300

    1

--- PROJELER/PRJ1/Form1.Designer.cs
+++ PROJELER/PRJ1/Form1.Designer.cs
@@ -28,13 +28,115 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.txtsayi1 = new System.Windows.Forms.TextBox();
+            this.txtsayi2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(12, 5);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(870, 559);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.groupBox1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(862, 533);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "1";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(862, 533);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "2";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // txtsayi1
+            // 
+            this.txtsayi1.Location = new System.Drawing.Point(16, 19);
+            this.txtsayi1.Name = "txtsayi1";
+            this.txtsayi1.Size = new System.Drawing.Size(145, 20);
+            this.txtsayi1.TabIndex = 0;
+            this.txtsayi1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtsayi1_KeyPress);
+            // 
+            // txtsayi2
+            // 
+            this.txtsayi2.Location = new System.Drawing.Point(16, 45);
+            this.txtsayi2.Name = "txtsayi2";
+            this.txtsayi2.Size = new System.Drawing.Size(145, 20);
+            this.txtsayi2.TabIndex = 1;
+            this.txtsayi2.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtsayi2_KeyPress);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ImageAlign = System.Drawing.ContentAlignment.TopRight;
+            this.label1.Location = new System.Drawing.Point(1, 134);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(209, 65);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "- Girilen 2 Sayının  Klavyeden \r\n        \"T\" basılınca Toplam\r\n        \"Ç\"  basıl" +
+    "ınca Çarpım vermesi   \r\n\r\n- Sadece nümerik sayı girilmesi validasyonu";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.txtsayi1);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.txtsayi2);
+            this.groupBox1.Location = new System.Drawing.Point(6, 6);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(217, 218);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Key Kontrol";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(894, 576);
+            this.Controls.Add(this.tabControl1);
+            this.KeyPreview = true;
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Form1_KeyPress);
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.TextBox txtsayi2;
+        private System.Windows.Forms.TextBox txtsayi1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- PROJELER/PRJ1/Form1.cs
+++ PROJELER/PRJ1/Form1.cs
@@ -16,5 +16,62 @@ namespace PRJ1
         {
             InitializeComponent();
         }
+
+        private void Form1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            string okunan = e.KeyChar.ToString().ToUpper();
+
+            if (okunan == "T" || okunan == "Ç")
+            {
+                if (string.IsNullOrEmpty(txtsayi1.Text))
+                {
+                    MessageBox.Show("Sayı 1 Girin");
+                    return;
+                }
+
+                if (string.IsNullOrEmpty(txtsayi2.Text))
+                {
+                    MessageBox.Show("Sayı 2 Girin");
+                    return;
+                }
+
+                int x = Convert.ToInt32(txtsayi1.Text);
+                int y = Convert.ToInt32(txtsayi2.Text);
+
+                if (okunan == "T")
+                {                 
+                    MessageBox.Show((x+y).ToString());
+                }
+
+                if (okunan == "Ç")
+                {                  
+                    MessageBox.Show((x*y).ToString());
+                }
+            }
+                
+
+           
+        }
+
+        private void txtsayi1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            Char okunanchar = e.KeyChar;    // txtsayi1 textbox giriş yapılır yapılmaz veriyi kontrol et
+
+            if (!char.IsDigit(okunanchar))  // okunan karakter digit değil ise
+            {
+                e.Handled = true; // textboxa girilmesini önle
+            }
+        }
+
+        private void txtsayi2_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            Char okunanchar = e.KeyChar;    // txtsayi2 textbox giriş yapılır yapılmaz veriyi kontrol et
+
+            if (!char.IsDigit(okunanchar))  // okunan karakter digit değil ise
+            {
+                e.Handled = true; // textboxa girilmesini önle
+            }
+
+        }
     }
 }
