d9c419c96f48a14170daa882fd285f99566c7720
pchsyn <pchsyn@DESKTOP-H5G5TTM>
2019-04-06 23:48:39 +0300

    sıralama

--- PROJELER/PRJ1/Form1.Designer.cs
+++ PROJELER/PRJ1/Form1.Designer.cs
@@ -35,9 +35,22 @@
             this.txtsayi2 = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.btnRandomUret1 = new System.Windows.Forms.Button();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.txtsirasayi1 = new System.Windows.Forms.TextBox();
+            this.btnEkle1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.List1 = new System.Windows.Forms.ListBox();
+            this.List2 = new System.Windows.Forms.ListBox();
+            this.btnkucuktobuyuk = new System.Windows.Forms.Button();
+            this.btnbuyuktokucuk = new System.Windows.Forms.Button();
             this.tabControl1.SuspendLayout();
             this.tabPage1.SuspendLayout();
             this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
             this.SuspendLayout();
             // 
             // tabControl1
@@ -52,6 +65,8 @@
             // 
             // tabPage1
             // 
+            this.tabPage1.Controls.Add(this.groupBox3);
+            this.tabPage1.Controls.Add(this.groupBox2);
             this.tabPage1.Controls.Add(this.groupBox1);
             this.tabPage1.Location = new System.Drawing.Point(4, 22);
             this.tabPage1.Name = "tabPage1";
@@ -91,12 +106,12 @@
             // 
             this.label1.AutoSize = true;
             this.label1.ImageAlign = System.Drawing.ContentAlignment.TopRight;
-            this.label1.Location = new System.Drawing.Point(1, 134);
+            this.label1.Location = new System.Drawing.Point(1, 97);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(209, 65);
             this.label1.TabIndex = 2;
-            this.label1.Text = "- Girilen 2 Sayının  Klavyeden \r\n        \"T\" basılınca Toplam\r\n        \"Ç\"  basıl" +
-    "ınca Çarpım vermesi   \r\n\r\n- Sadece nümerik sayı girilmesi validasyonu";
+            this.label1.Text = "- Girilen 2 sayı için\r\n        \"T\" basılınca Toplam\r\n        \"Ç\"  basılınca Çarpı" +
+    "m vermesi   \r\n\r\n- Sadece nümerik sayı girilmesi validasyonu";
             // 
             // groupBox1
             // 
@@ -105,10 +120,118 @@
             this.groupBox1.Controls.Add(this.txtsayi2);
             this.groupBox1.Location = new System.Drawing.Point(6, 6);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(217, 218);
+            this.groupBox1.Size = new System.Drawing.Size(217, 184);
             this.groupBox1.TabIndex = 3;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Key Kontrol";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.btnRandomUret1);
+            this.groupBox2.Location = new System.Drawing.Point(229, 6);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(167, 56);
+            this.groupBox2.TabIndex = 4;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Random Sayı Üret";
+            // 
+            // btnRandomUret1
+            // 
+            this.btnRandomUret1.Location = new System.Drawing.Point(21, 20);
+            this.btnRandomUret1.Name = "btnRandomUret1";
+            this.btnRandomUret1.Size = new System.Drawing.Size(132, 23);
+            this.btnRandomUret1.TabIndex = 0;
+            this.btnRandomUret1.Text = "TIKLA";
+            this.btnRandomUret1.UseVisualStyleBackColor = true;
+            this.btnRandomUret1.Click += new System.EventHandler(this.btnRandomUret1_Click);
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.List2);
+            this.groupBox3.Controls.Add(this.List1);
+            this.groupBox3.Controls.Add(this.label3);
+            this.groupBox3.Controls.Add(this.label2);
+            this.groupBox3.Controls.Add(this.btnbuyuktokucuk);
+            this.groupBox3.Controls.Add(this.btnkucuktobuyuk);
+            this.groupBox3.Controls.Add(this.btnEkle1);
+            this.groupBox3.Controls.Add(this.txtsirasayi1);
+            this.groupBox3.Location = new System.Drawing.Point(402, 6);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(236, 352);
+            this.groupBox3.TabIndex = 5;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Sıralama";
+            // 
+            // txtsirasayi1
+            // 
+            this.txtsirasayi1.Location = new System.Drawing.Point(6, 22);
+            this.txtsirasayi1.Name = "txtsirasayi1";
+            this.txtsirasayi1.Size = new System.Drawing.Size(127, 20);
+            this.txtsirasayi1.TabIndex = 0;
+            // 
+            // btnEkle1
+            // 
+            this.btnEkle1.Location = new System.Drawing.Point(139, 20);
+            this.btnEkle1.Name = "btnEkle1";
+            this.btnEkle1.Size = new System.Drawing.Size(75, 23);
+            this.btnEkle1.TabIndex = 1;
+            this.btnEkle1.Text = "EKLE";
+            this.btnEkle1.UseVisualStyleBackColor = true;
+            this.btnEkle1.Click += new System.EventHandler(this.btnEkle1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(8, 67);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(69, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "POZİTİFLER";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(143, 69);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(74, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "NEGATİFLER";
+            // 
+            // List1
+            // 
+            this.List1.FormattingEnabled = true;
+            this.List1.Location = new System.Drawing.Point(10, 83);
+            this.List1.Name = "List1";
+            this.List1.Size = new System.Drawing.Size(96, 173);
+            this.List1.TabIndex = 3;
+            // 
+            // List2
+            // 
+            this.List2.FormattingEnabled = true;
+            this.List2.Location = new System.Drawing.Point(117, 83);
+            this.List2.Name = "List2";
+            this.List2.Size = new System.Drawing.Size(98, 173);
+            this.List2.TabIndex = 3;
+            // 
+            // btnkucuktobuyuk
+            // 
+            this.btnkucuktobuyuk.Location = new System.Drawing.Point(10, 265);
+            this.btnkucuktobuyuk.Name = "btnkucuktobuyuk";
+            this.btnkucuktobuyuk.Size = new System.Drawing.Size(204, 23);
+            this.btnkucuktobuyuk.TabIndex = 1;
+            this.btnkucuktobuyuk.Text = "SIRALA KUCUKTEN BUYUGE";
+            this.btnkucuktobuyuk.UseVisualStyleBackColor = true;
+            this.btnkucuktobuyuk.Click += new System.EventHandler(this.btnkucuktobuyuk_Click);
+            // 
+            // btnbuyuktokucuk
+            // 
+            this.btnbuyuktokucuk.Location = new System.Drawing.Point(6, 294);
+            this.btnbuyuktokucuk.Name = "btnbuyuktokucuk";
+            this.btnbuyuktokucuk.Size = new System.Drawing.Size(204, 23);
+            this.btnbuyuktokucuk.TabIndex = 1;
+            this.btnbuyuktokucuk.Text = "SIRALA BUYUKTEN KUCUGE";
+            this.btnbuyuktokucuk.UseVisualStyleBackColor = true;
+            this.btnbuyuktokucuk.Click += new System.EventHandler(this.btnbuyuktokucuk_Click);
             // 
             // Form1
             // 
@@ -124,6 +247,9 @@
             this.tabPage1.ResumeLayout(false);
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -137,6 +263,17 @@
         private System.Windows.Forms.TextBox txtsayi1;
         private System.Windows.Forms.GroupBox groupBox1;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button btnRandomUret1;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.ListBox List2;
+        private System.Windows.Forms.ListBox List1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnkucuktobuyuk;
+        private System.Windows.Forms.Button btnEkle1;
+        private System.Windows.Forms.TextBox txtsirasayi1;
+        private System.Windows.Forms.Button btnbuyuktokucuk;
     }
 }
 
--- PROJELER/PRJ1/Form1.cs
+++ PROJELER/PRJ1/Form1.cs
@@ -67,9 +67,119 @@ namespace PRJ1
         {
             Char okunanchar = e.KeyChar;    // txtsayi2 textbox giriş yapılır yapılmaz veriyi kontrol et
 
-            if (!char.IsDigit(okunanchar))  // okunan karakter digit değil ise
+            if (!char.IsDigit(okunanchar) && okunanchar != 8)  // okunan karakter digit değil ve silme tuşu değil ise 
             {
                 e.Handled = true; // textboxa girilmesini önle
+            }
+
+        }
+
+        private void btnRandomUret1_Click(object sender, EventArgs e)
+        {
+            Random rnd = new Random();
+            int x=rnd.Next(0, 100);
+            btnRandomUret1.Text = x.ToString();
+
+        }
+
+        private void btnEkle1_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(txtsirasayi1.Text))
+                return;
+
+            int okunan = Convert.ToInt32(txtsirasayi1.Text);
+
+            if (okunan < 0)
+            {
+                List2.Items.Add(okunan.ToString());
+            }
+            else
+            {
+                List1.Items.Add(okunan.ToString());
+            }
+        }
+
+      
+        private void btnbuyuktokucuk_Click(object sender, EventArgs e)
+        {
+            int gecici;
+
+            /////// Pozitif Liste
+            for (int i = 0; i < List1.Items.Count; i++)
+            {
+                int x = Convert.ToInt32(List1.Items[i]);
+
+                for (int k = i; k < List1.Items.Count; k++)
+                {                  
+                    int y = Convert.ToInt32(List1.Items[k]);
+                    if (x < y)
+                    {
+                        gecici = y;
+                        List1.Items[k] = List1.Items[i];
+                        List1.Items[i] = gecici;
+                    }
+                }
+            }
+
+            /////// Negatif Liste
+            int gecici2;
+            for (int i = 0; i < List2.Items.Count; i++)
+            {
+                int x = Convert.ToInt32(List2.Items[i]);
+
+                for (int k = i; k < List2.Items.Count; k++)
+                {
+                    int y = Convert.ToInt32(List2.Items[k]);
+                    if (x < y)
+                    {
+                        gecici2 = y;
+                        List2.Items[k] = List2.Items[i];
+                        List2.Items[i] = gecici2;
+                    }
+                }
+            }
+        }
+
+        private void btnkucuktobuyuk_Click(object sender, EventArgs e)
+        {
+            int gecici;
+
+            /////// Pozitif Liste
+
+            for (int i = 0; i < List1.Items.Count; i++)
+            {
+                int x = Convert.ToInt32(List1.Items[i]);
+
+                for (int k =i; k < List1.Items.Count; k++)
+                {                  
+                    int y = Convert.ToInt32(List1.Items[k]);
+
+                    if (x > y)
+                    {
+                        gecici = y;
+                        List1.Items[k] = List1.Items[i];
+                        List1.Items[i] = gecici;
+                    }
+                }
+            }
+
+            /////// Negatif Liste
+
+            int gecici2;
+            for (int i = 0; i < List2.Items.Count; i++)
+            {
+                int x = Convert.ToInt32(List2.Items[i]);
+
+                for (int k = i; k < List2.Items.Count; k++)
+                {
+                    int y = Convert.ToInt32(List2.Items[k]);
+                    if (x > y)
+                    {
+                        gecici2 = y;
+                        List2.Items[k] = List2.Items[i];
+                        List2.Items[i] = gecici2;
+                    }
+                }
             }
 
         }
