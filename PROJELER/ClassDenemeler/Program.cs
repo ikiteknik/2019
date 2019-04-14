f948e343d6ea7a626de7af46ba6579edc78c6d14
pchsyn <pchsyn@DESKTOP-H5G5TTM>
2019-04-14 11:30:14 +0300

    pazar dersi

---
+++ PROJELER/ClassDenemeler/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ClassDenemeler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("class ogrenme");
+
+            Ogrenci ogr1 = new Ogrenci();
+            ogr1.OgrenciAdi = "huseyin";
+            ogr1.OgrenciSoyadi = "Atmaca";
+
+
+            int sonc = ogr1.ToplamaMetod(8, 9);
+            string sonc1 = ogr1.ToplamaMetod("8","9");
+
+            Console.WriteLine(sonc.ToString());
+            Console.WriteLine(sonc1.ToString());
+            Console.ReadKey();
+        }
+    }
+    public class Ogrenci
+    {
+        //fields
+        //constructor method
+        //normal methods
+        //property
+        //events
+        //delegate
+        //nested class
+
+
+        /////// fields
+        public string OgrenciAdi;
+        public string OgrenciSoyadi;
+
+        /////// constructor (yapıcı metod)
+        public Ogrenci()
+        {
+            //MessageBox.Show("constructor Class Olustu");
+        }
+
+        /////// deconstructor (yapıcı metod)
+        ~Ogrenci()
+        {
+            //MessageBox.Show("Class öldü");
+        }
+
+        /////// normal metod
+        public int NormalMetod()
+        {
+            return 999;
+        }
+
+        public int ToplamaMetod(int say1, int sayi2)
+        {
+            return say1 + sayi2;
+        }
+
+        public string ToplamaMetod(string say1, string sayi2)
+        {
+            return say1 + sayi2;
+        }
+
+    }
+
+
+}
---
+++ PROJELER/Kalitim/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kalitim
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            A a = new A();
+
+            Console.ReadKey();
+        }
+    }
+
+
+
+    public class A : B
+    {
+        public string averisi;
+
+        public A()
+        {
+            Console.WriteLine("A Klasım oluştu");
+        }
+    }
+
+
+    public class B : C
+    {
+
+        public string bverisi;
+        public B()
+        {
+            Console.WriteLine("B Klasım oluştu");
+        }
+    }
+
+    public class C:D
+    {
+        public C()
+        {
+            Console.WriteLine("C Klass Oluştu");
+        }
+
+        public C(string c)
+        {
+            Console.WriteLine("C parametreli cons. oluştu oluştu {0}", c);
+
+        }
+    }
+
+    public  class D
+    {
+
+    }
+
+
+
+
+}
