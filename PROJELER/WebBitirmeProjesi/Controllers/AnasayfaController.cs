1d6d55df27341a9cc7c8ad6d2abfdcc88f59c7c7
Pc6 <Pc6@PC6-BILGISAYAR>
2019-05-19 11:06:50 +0300

    pazar 2

---
+++ PROJELER/WebBitirmeProjesi/Controllers/AnasayfaController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebBitirmeProjesi.Controllers
+{
+    public class AnasayfaController : Controller
+    {
+        // GET: Anasayfa
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+    }
+}
