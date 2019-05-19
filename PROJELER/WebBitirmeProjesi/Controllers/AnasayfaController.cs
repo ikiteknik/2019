9ad295fa6b41452c20ddb652a84be246ce751606
Pc6 <Pc6@PC6-BILGISAYAR>
2019-05-19 13:06:34 +0300

    db bbitti

--- PROJELER/WebBitirmeProjesi/Controllers/AnasayfaController.cs
+++ PROJELER/WebBitirmeProjesi/Controllers/AnasayfaController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using WebBitirmeProjesi.DbEntity;
 
 namespace WebBitirmeProjesi.Controllers
 {
@@ -11,6 +12,18 @@ namespace WebBitirmeProjesi.Controllers
         // GET: Anasayfa
         public ActionResult Index()
         {
+            LOKANTAEntities db = new LOKANTAEntities();
+            db.KISILER.ToList();
+            List<HABER> y = db.HABER.ToList();
+
+            string habericerk = y.Where(x => x.HaberId == 1).FirstOrDefault().HaberIcerik;
+            string haberbaslik = y.Where(x => x.HaberId == 1).FirstOrDefault().HaberBaslik;
+
+            ViewBag.Icerik = habericerk;
+            ViewBag.Baslik = haberbaslik;
+
+
+
             return View();
         }
 
